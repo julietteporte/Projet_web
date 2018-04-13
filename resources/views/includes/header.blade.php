04defef8f911c5039841c3f84dd8cc0883b2bc2a
julietteporte <juliette.porte@viacesi.fr>
2018-04-13 14:49:06 +0200

    Rectification
    Rectification

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,56 +1,56 @@
-<link rel="stylesheet"
-href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
-<link rel="stylesheet" href="style.css" />
-<script
-src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script
-src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+<link rel="stylesheet" href="css/style.css" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
-<nav>
+<style>
+header
+{
+    background : url('pictures/baniere.jpg')
+}
+</style>
+
+<header>
 	<div class="container-fluid">
 		<div class="navbar-header">
-			<a href='/'><img src="pictures/logo.png" width="250" height="250"></a>
+			<img src="pictures/logo.png" width="200" height="200">
 		</div>
 		<div class="container-fluid">
 			<ul class="nav navbar-nav navbar-right">
-				<li><a href="#" style='color: #6E6E6E;'><span
-						class="glyphicon glyphicon-shopping-cart" title="Panier"></span></a></li>
-				<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" style='color: #6E6E6E;'><span class="glyphicon glyphicon-user"></span>
-						Mon compte <span class="caret"></span></a></a>
-					<ul class="dropdown-menu" role="menu">
-						<li><a href="#">Mon compte</a></li>
-						<li><a href="#">Déconnexion</a></li>
+				<li><a href="#" style='color: #FFFFFF;'><span class="glyphicon glyphicon-shopping-cart" title="Panier"></span></a></li>
+					<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" style='color: #FFFFFF;'><span class="glyphicon glyphicon-user"></span> Mon compte <span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu">
+							<li><a href="#">Mon compte</a></li>
+							<li><a href="#">Déconnexion</a></li>
+						</ul></li>
 					</ul>
-				</li>
-			</ul>
-			<br> <br> <br> <br> <br>
-			<div class="container" align="center">
-				<form class="navbar-form navbar-search" role="search" method="post">
-					<div class="form-group">
-						<div class="dropdown">
-							<div class="form-group has-feedback">
-								<input type="text" class="form-control" placeholder="Rechercher"
-									name="keywords">
+					<br> <br> <br> <br> <br>
+					<div class="container" align="center">
+						<form class="navbar-form navbar-search" role="search" method="post">
+							<div class="form-group">
+								<div class="dropdown">
+									<div class="form-group has-feedback">
+										<input type="text" class="form-control" placeholder="Rechercher" name="keywords">
+									</div>
+									<button type="submit" class="btn" style='color: #6E6E6E;'>
+										<span class="glyphicon glyphicon-search"></span>
+									</button>
+								</div>
 							</div>
-							<button type="submit" class="btn" style='color: #6E6E6E;'>
-								<span class="glyphicon glyphicon-search"></span>
-							</button>
-						</div>
+						</form>
 					</div>
-				</form>
-			</div>
-		</div>
-		<div class="row">
-			<nav class="navbar navbar-default" role="navigation">
-				<ul class="nav nav-pills nav-justified">
-					<li><a href="#" class="text-danger"><h4>ACCUEIL</h4></a></li>
-					<li><a href="#" class="text-danger"><h4>QUI SOMMES NOUS ?</h4></a></li>
-					<li><a href="#" class="text-danger"><h4>BOUTIQUE</h4></a></li>
-					<li><a href="#" class="text-danger"><h4>BOITE A IDEES</h4></a></li>
-					<li><a href="#" class="text-danger"><h4>MANIFESTATION</h4></a></li>
-					<li><a href="#" class="text-danger"><h4>PARTENAIRES</h4></a></li>
-				</ul>
-			</nav>
-		</div>
-	</div>
-</nav>
+				</div>
+				<div class="row">
+					<nav class="navbar navbar-default" role="navigation">
+						<ul class="nav nav-pills nav-justified">
+							<li><a href="#" class="text-muted"><h4>ACCUEIL</h4></a></li>
+							<li><a href="#" class="text-muted"><h4>QUI SOMMES NOUS</h4></a></li>
+								<li><a href="#" class="text-muted"><h4>BOUTIQUE</h4></a></li>
+								<li><a href="#" class="text-muted"><h4>BOITE A IDEES</h4></a></li>
+								<li><a href="#" class="text-muted"><h4>MANIFESTATION</h4></a></li>
+								<li><a href="#" class="text-muted"><h4>PARTENAIRES</h4></a></li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</header>
