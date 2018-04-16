0c6f9b468a1905c728001bf895a2be55a7ade8d0
darksheina <darksheina@hotmail.fr>
2018-04-16 16:13:12 +0200

    footer modif
    footer modif

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -4,27 +4,27 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+
 <footer>
   <hr>
   <div class="social-media" align='center' >
     <div class="panel-body" >
-      <a href="https://www.facebook.com/bdecesibdx/"><i class="fa fa-facebook" style='color:#8A0808'></i><b> Bde Cesi Bordeaux</b></a>
+      <a href="https://www.facebook.com/bdecesibdx/"><i class="fa fa-facebook" class="text-muted"></i><b> Bde Cesi Bordeaux</b></a>
       <div class="form-group col-lg-8 col-md-8">
-        <a href="https://twitter.com/BdeCesiBordeaux"><i class="fa fa-twitter" style='color:#8A0808'></i><b> @BdeCesiBordeaux</b></a>
+        <a href="https://twitter.com/BdeCesiBordeaux"><i class="fa fa-twitter" class="text-muted"></i><b> @BdeCesiBordeaux</b></a>
         <div class="form-group col-lg-8 col-md-8">
-          <i class="fa fa-snapchat-ghost" style='color:#8A0808'></i><b> cesibordeaux</b>
+          <i class="fa fa-snapchat-ghost" class="text-muted"></i><b> cesibordeaux</b>
         </div>
       </div>
     </div>
-
-    <div class="well well" align="center">
-      <a href="/confidentiality" style='color:#8A0808'>{{__('footer.policy') }}</a>
+    <div class="well well" align="center" style="background: url('../pictures/baniere.jpg');">
+      <a href="/confidentiality" style='color:#FFFFFF'>{{__('footer.policy') }}</a>
       <div class="form-row col-lg-8 col-md-8">
-        <a href="/legal_notice" style='color:#8A0808'>{{__('footer.notice') }}</a>
+        <a href="/legal_notice" style='color:#FFFFFF'>{{__('footer.notice') }}</a>
         <div class="form-group col-lg-7 col-md-7">
-          <a href="/terms_conditions" style='color:#8A0808'>{{__('footer.CG') }}</a>
+          <a href="/terms_conditions" style='color:#FFFFFF'>{{__('footer.CG') }}</a>
           <div class="form-group col-lg-7 col-md-7">
-            <span class="glyphicon glyphicon-envelope"></span><a href="/contact_us" style='color:#8A0808'><b> {{__('footer.contactUs') }}</b></a>
+            <span class="glyphicon glyphicon-envelope" style='color:#FFFFFF'></span><a href="/contact_us" style='color:#FFFFFF'><b> {{__('footer.contactUs') }}</b></a>
           </div>
         </div>
       </div>
