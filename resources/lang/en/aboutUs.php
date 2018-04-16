4078c2ebe4c2bf99779a758af09f0386c3b7dbc5
darksheina <darksheina@hotmail.fr>
2018-04-16 11:12:28 +0200

    translation part 4
    translation part 4

---
+++ resources/lang/en/aboutUs.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title' => 'The BDE',
+    'utility' => 'What is a BDE?',
+    'text1' => 'A student office serves many purposes but its main role is to listen, help and animate.',
+    'text2' => 'Listen to students\' needs but also help them to complete their projects, and finally animate the student life of the school.',
+    'text3' => 'Also, if you need help do not hesitate to contact us and we will be happy to help you.',
+    'team' => 'The full team',
+];
