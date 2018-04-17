64fa737abafdf36c696322a294700342d6baf241
julietteporte <juliette.porte@viacesi.fr>
2018-04-17 18:33:30 +0200

    travail page boutique
    travail page boutique

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -8,40 +8,68 @@
 </header>
 <div class="row">
   <div class="col-lg-12 col-md-12">
-    <div>
-      <br />
-      <h2 class="text-center">{{__ ('shop.article')}}</h2>
-      <br />
-    </div>
+    @if(Auth::user()->ID_TypeCompte === 2)<br/><br/>
+    <button type="submit" class="btn btn-default" align="center">Ajouter un produit</button>
+    @endif
+    <br/><br/>
+    <h1 class="text-center">{{__ ('shop.article')}}</h1>
+    <br/><br/>
   </div>
+</div>
 
-  <div class="col-lg-9 col-md-9">
-    @foreach ($produit as $p)
-    <div class="col-lg-4 col-md-4">
-      <div class="panel panel-default">
-        <div class="panel-body">
-          <div class="form-row">
-            <div class="form-group col-lg-8 col-md-8 text-center">
-              <img src="/uploads/{{ $p->Fichier }}" alt="image" height="200" width="320">
-            </div>
-            <ul>
-              <div class="form-group col-lg-4 col-md-4">
-                <a href="/shop/{{ $p->ID }}"><br/><br/><h4><b>{{ $p->Intitule }}</b></h4></a>
-                <label class="text-danger"><h4>{{ $p->Prix }} € TTC</h4></label>
-              </div>
-              <div class="form-group col-lg-12 col-md-5">
-                <p>{{ $p->Legende }}</p>
-              </div>
-            </ul>
+<div class="col-lg-9 col-md-9">
+  @foreach ($produit as $p)
+  <div class="col-lg-4 col-md-4">
+    <div class="panel panel-default">
+      <div class="panel-body">
+        <div class="form-row">
+          <div class="form-group col-lg-8 col-md-8 text-center">
+            <img src="/uploads/{{ $p->Fichier }}" alt="image" height="200" width="320">
           </div>
+          <ul>
+            <div class="form-group col-lg-4 col-md-4">
+              @if(Auth::user()->ID_TypeCompte === 2)
+              <span class="glyphicon glyphicon-pencil" aria-hidden="true" text-right></span>
+              @endif
+              <a href="/shop/{{ $p->ID }}"><br/><br/><h4><b>{{ $p->Intitule }}</b></h4></a>
+              <label class="text-danger"><h4>{{ $p->Prix }} € TTC</h4></label>
+              <br><br>
+              @if(Auth::user()->ID_TypeCompte === 2)
+              <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
+              @endif
+            </div>
+            <div class="form-group col-lg-12 col-md-5">
+              <p>{{ $p->Legende }}</p>
+            </div>
+          </ul>
         </div>
       </div>
     </div>
-    @endforeach
   </div>
-  <div class="col-lg-3 col-md-3">
-    <div>
-      <h3>{{__ ('shop.mostCommanded')}}</h3>
+  @endforeach
+</div>
+<div class="col-lg-3 col-md-3">
+  <div>
+    <h3>{{__ ('shop.mostCommanded')}}</h3>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-body">
+      <div class="form-group col-lg-3 col-md-3">
+        <img src="file:///D:/workspace_php/siteBDE/image/image.jpg"
+        alt="image" height="60" width="60">
+      </div>
+      <div class="form-group col-lg-6 col-md-6">
+        <label>Nom</label> <br /> <br /> <label class="text-danger">Prix
+          € TTC</label> <label>Légende produit</label>
+          <p>Brève description produit</p>
+        </div>
+        <div class="form-group col-lg-3 col-md-3">
+          <button class="btn" role="button">
+            + <span class="glyphicon glyphicon-shopping-cart"
+            aria-hidden="true"></span>
+          </button>
+        </div>
+      </div>
     </div>
     <div class="panel panel-default">
       <div class="panel-body">
@@ -81,27 +109,8 @@
             </div>
           </div>
         </div>
-        <div class="panel panel-default">
-          <div class="panel-body">
-            <div class="form-group col-lg-3 col-md-3">
-              <img src="file:///D:/workspace_php/siteBDE/image/image.jpg"
-              alt="image" height="60" width="60">
-            </div>
-            <div class="form-group col-lg-6 col-md-6">
-              <label>Nom</label> <br /> <br /> <label class="text-danger">Prix
-                € TTC</label> <label>Légende produit</label>
-                <p>Brève description produit</p>
-              </div>
-              <div class="form-group col-lg-3 col-md-3">
-                <button class="btn" role="button">
-                  + <span class="glyphicon glyphicon-shopping-cart"
-                  aria-hidden="true"></span>
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
-      <footer class="row">
-        @include('includes.footer')
-      </footer>
+    </div>
+    <footer class="row">
+      @include('includes.footer')
+    </footer>
